33d7818874dd7a2f80b5ac0b65437f4552a4da5e
bilkn <65674342+bilkn@users.noreply.github.com>
2020-12-24 11:36:35 +0300

    fix: update book component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// CSS 
-import "./index.css";
+// CSS
+import './index.css';
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
     </section>
   );
@@ -18,20 +13,15 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/71pUK2k0uHL._AC_UL320_.jpg"
+        alt="Dune"
+      />
+      <h1 style={{ marginBottom: '1rem' }}>
+        Dune: Book 1 (Dune: the Graphic Novel) (Volume 1)
+      </h1>
+      <h4>Frank Herbert</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/71pUK2k0uHL._AC_UL320_.jpg"
-    alt="Dune"
-  />
-);
-
-const Title = () => <h1>Dune: Book 1 (Dune: the Graphic Novel) (Volume 1)</h1>;
-const Author = () => <h4>Frank Herbert</h4>;
 ReactDOM.render(<BookList />, document.getElementById('root'));
